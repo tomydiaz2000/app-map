dbb594320488fbfa0f80b85ed1d6072d20adbbbe
Tomas Diaz <tomas.diaz@cintelink.com.ar>
2021-10-04 23:43:05 -0300

    componente reutilizable headerButton

--- screens/PlaceListScreen.js
+++ screens/PlaceListScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import HeaderButton from '../components/HeaderButton'
 
 const PlaceListScreen = () => {
     return (
