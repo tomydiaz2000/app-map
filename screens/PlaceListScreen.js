3d3715e2a8cb7c7778e41ffe750631c812784b9c
Tomas Diaz <tomas.diaz@cintelink.com.ar>
2021-10-08 15:58:11 -0300

    sqlite

--- screens/PlaceListScreen.js
+++ screens/PlaceListScreen.js
@@ -1,45 +1,48 @@
-import React, { useLayoutEffect } from 'react'
-import { View, Text, StyleSheet, FlatList } from 'react-native'
-import { useSelector } from 'react-redux'
-import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+import React, { useLayoutEffect } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { useSelector } from "react-redux";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
-import HeaderButton from '../components/HeaderButton'
-import PlaceItem from '../components/PlaceItem'
+import HeaderButton from "../components/HeaderButton";
+import PlaceItem from "../components/PlaceItem";
 
 const PlaceListScreen = ({ navigation }) => {
-    const places = useSelector(state => state.places.places)
-    console.log(places)
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerRight: () => (
-                <HeaderButtons HeaderButtonComponent={HeaderButton}>
-                    <Item 
-                        title="Nueva"
-                        iconName="md-add"
-                        onPress={() => navigation.navigate('Nuevo')} />
-                </HeaderButtons>
-            )
-        })
-    }, [navigation])
+  const places = useSelector((state) => state.places.places);
+  console.log(places);
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+          <Item
+            title="Nueva"
+            iconName="md-add"
+            onPress={() => navigation.navigate("Nuevo")}
+          />
+        </HeaderButtons>
+      ),
+    });
+  }, [navigation]);
 
-    return (
-        <FlatList data={places}
-            keyExtract={item => item.id}
-            renderItem={itemData => (
-                <PlaceItem 
-                image={itemData.item.image}
-                title={itemData.item.title}
-                address={null}
-                onSelect={() => {}}/>
-            )}>
-        </FlatList>
-    )
-}
+  return (
+    <FlatList
+      data={places}
+      keyExtract={(item) => item.id}
+      renderItem={(itemData) => (
+        <PlaceItem
+          image={itemData.item.image}
+          title={itemData.item.title}
+          address={null}
+          onSelect={() => {}}
+        />
+      )}
+    ></FlatList>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
-})
+  container: {
+    flex: 1,
+  },
+});
 
-export default PlaceListScreen
+export default PlaceListScreen;
